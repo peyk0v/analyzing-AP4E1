966c0e0efa6fc944bd2b35df431f3282e37d2833
peyk0v <peykovn@gmail.com>
2024-06-17 22:46:40 -0300

    fix: ahora hago un reverse, igualmente el marco 'importante' es el 1

--- parte 1/script.pm
+++ parte 1/script.pm
@@ -3,25 +3,25 @@ use strict;
 use warnings;
 use Bio::SeqIO;
 
-my $genbank_file = 'AP4E1.gb';
+my $genbank_file = './AP4E1.gb';
+my $output_file= './output.fasta';
 
 my $seqio_obj = Bio::SeqIO->new(-file => $genbank_file, -format => "genbank");
-
-my $fasta_out = Bio::SeqIO->new(-file => '>output.fasta', -format => 'fasta');
+my $fasta_out = Bio::SeqIO->new(-file => $output_file, -format => 'fasta');
 
 while (my $seq_obj = $seqio_obj->next_seq) {
-    #para los frames 0, 1 y 2
-	for my $frame (0..2) {
-    	my $pro_seq_obj = $seq_obj->translate(-frame => $frame);
-    	$fasta_out->write_seq($pro_seq_obj);
-	}
+  # Traducción para marcos de lectura 0, 1 y 2
+  for my $frame (0..2) {
+    my $pro_seq_obj = $seq_obj->translate(-frame => $frame);
+    $fasta_out->write_seq($pro_seq_obj);
+  }
 
-    #para los frames -1 y -2
-    my $rev_seq = $seq_obj->revcom;
-    for my $frame (0..2) {
-        my $pro_seq_obj = $rev_seq->translate(-frame => $frame);
-        $fasta_out->write_seq($pro_seq_obj);
-    }
+  # Traducción para marcos de lectura reverseados 
+  my $rev_seq = $seq_obj->revcom;
+  for my $frame (0..2) {
+    my $pro_seq_obj = $rev_seq->translate(-frame => $frame);
+    $fasta_out->write_seq($pro_seq_obj);
+  }
 }
 
-print "Secuencias de aminoácidos traducidas\n";
+print "Secuencias de aminoácidos traducidas y escritas en output.fasta\n";
