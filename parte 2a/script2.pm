450446c2c529c62898decfe859d181ced66bc2db
peyk0v <peykovn@gmail.com>
2024-06-17 22:47:01 -0300

    genero dos Blasts con diferentes formatos

--- parte 2a/script2.pm
+++ parte 2a/script2.pm
@@ -4,9 +4,9 @@ use warnings;
 
 my $input_file = 'input.fasta';
 
-my $output_file = 'blast.out';
+my $output_file = 'blast2.out';
 
-my $blastp_cmd = "blastp -query $input_file -db ../../../Blast/ncbi-blast-2.15.0+/data/swissprot -out $output_file -outfmt 5";
+my $blastp_cmd = "blastp -query $input_file -db ../../../BioInformatica/Blast/ncbi-blast-2.15.0+/data/swissprot -out $output_file";
 
 system($blastp_cmd);
 
